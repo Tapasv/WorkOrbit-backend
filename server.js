d6b6a8e82f9f5f6929e22534059ee35b431ba87b
Tapasv <tapasvkaushal@gmail.com>
2026-01-06 14:37:29 +0530

    fix

--- server.js
+++ server.js
@@ -29,14 +29,14 @@ app.use(cors(corsOptions));
 
 app.use(express.json())
 
-// app.get('/api/health', (req, res) => {
-//   res.status(200).json({ 
-//     status: 'ok', 
-//     message: 'Server is running',
-//     timestamp: new Date(),
-//     uptime: process.uptime()
-//   });
-// });
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ 
+    status: 'ok', 
+    message: 'Server is running',
+    timestamp: new Date(),
+    uptime: process.uptime()
+  });
+});
 
 DBconnect()
 
